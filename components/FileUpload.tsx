f15aea1243972978eb7a5af360342e2c213e8e40
licraw <lcrawshaw@fender.com>
2023-01-29 17:38:40 -0800

    update

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 interface FileUploaderProps {
   onFileAdded: (file: File) => void;
@@ -24,56 +24,69 @@ const QuizWrapper = styled.div`
 `;
 
 const UploadWrapper = styled.div`
-border: 2px dashed #ccc;
-border-radius: 5px;
-padding: 50px;
-text-align: center;
+  border: 2px dashed #ccc;
+  border-radius: 5px;
+  padding: 50px;
+  text-align: center;
 `;
 
 const FileUploader: React.FC<FileUploaderProps> = ({ onFileAdded }) => {
-    const [htmlContent, setHtmlContent] = useState("");
-    const handleFileAdded = useCallback((file: File) => {
-        // Check file type
-        if (file.type !== "text/html") {
-            alert("Invalid file type. Please select an HTML file.");
-            return;
-        }
-    
-        // Check file size
-        const maxFileSize = 1e6; // 1 MB
-        if (file.size > maxFileSize) {
-            alert(`File size too large. Please select a file less than ${maxFileSize / 1e6} MB.`);
-            return;
-        }
-    
-        // Prepare file for sending to endpoint
-        const formData = new FormData();
-        formData.append("file", file);
-    
-        // Send file to endpoint
-        fetch("http://localhost:3001", {
-            method: "POST",
-            body: formData
-        })
-        .then(response => response.text())
-        .then(data => {
-            setHtmlContent(data);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    
-    }, [onFileAdded]);
-    
+  const [htmlContent, setHtmlContent] = useState("");
+  const [sendData, setSendData] = useState(false);
 
-  const onDrop = useCallback(
-    (acceptedFiles: File[]) => {
-      if (acceptedFiles.length > 0) {
-        handleFileAdded(acceptedFiles[0]);
+  const handleFileAdded = async (files: File[]) => {
+    let htmlContent = "";
+    for (const file of files) {
+      if (file.type !== "text/html") {
+        alert("Invalid file type. Please select an HTML file.");
+        return;
       }
-    },
-    [handleFileAdded]
-  );
+
+      // Check file size
+      const maxFileSize = 1e6; // 1 MB
+      if (file.size > maxFileSize) {
+        alert(
+          `File size too large. Please select a file less than ${
+            maxFileSize / 1e6
+          } MB.`
+        );
+        return;
+      }
+
+      /// Read file content
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        htmlContent += e.target.result;
+      };
+      reader.readAsText(file);
+    }
+    console.log(files);
+    setHtmlContent(htmlContent);
+    console.log(htmlContent);
+  };
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    handleFileAdded(acceptedFiles);
+  }, []);
+
+  useEffect(() => {
+    if (sendData) {
+      fetch("http://localhost:3001", {
+        method: "POST",
+        body: JSON.stringify({ html: htmlContent }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.text())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [sendData, htmlContent]);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
@@ -81,13 +94,19 @@ const FileUploader: React.FC<FileUploaderProps> = ({ onFileAdded }) => {
     <PageWrapper {...getRootProps()}>
       <input {...getInputProps()} />
       <UploadWrapper>
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag and drop some files here, or click to select files</p>
-      )}
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag and drop some files here, or click to select files</p>
+        )}
       </UploadWrapper>
-     <QuizWrapper>{htmlContent && <div dangerouslySetInnerHTML={{ __html: htmlContent }} />}</QuizWrapper>
+      <br></br>
+      <button onClick={()=>setSendData}>Build Study Guide</button>
+      <QuizWrapper>
+        {htmlContent && (
+          <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+        )}
+      </QuizWrapper>
     </PageWrapper>
   );
 };
